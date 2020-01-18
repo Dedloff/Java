8127f84ee65e70c171f598fbadbe33f1eb4cad00
Dedloff <dedloffn@gmail.com>
2020-01-18 11:21:29 -0500

    Hour 2
    Simple Variable, Class, and Package example

---
+++ Hour 2/src/salution/Salution.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package salution;
+
+/**
+ * @author administrator
+ *
+ */
+public class Salution {
+
+	public static void main(String[] args) {
+		String greeting = "Saluton mondo!";
+		System.out.println(greeting);;
+
+	}
+
+}
