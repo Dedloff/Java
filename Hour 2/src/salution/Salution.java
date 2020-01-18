a7796e40520618185cccb4ef975434578ee16604
Dedloff <dedloffn@gmail.com>
2020-01-18 11:25:08 -0500

    Update Salution.java

--- Hour 2/src/salution/Salution.java
+++ Hour 2/src/salution/Salution.java
@@ -2,17 +2,22 @@
  * 
  */
 package salution;
+//Package name is salution 
 
 /**
  * @author administrator
  *
  */
 public class Salution {
+	//class Salution also must have the same filename Salution.java
 
 	public static void main(String[] args) {
 		String greeting = "Saluton mondo!";
+		//Storing information as a variable then printing out the variable on the next line
 		System.out.println(greeting);;
 
+		
+		
 	}
 
 }
