c110b3f7f59b254596f2fe5d85d97937d22b5723
Dedloff <dedloffn@gmail.com>
2020-01-19 17:08:49 -0500

    Add files via upload

--- Hour 3/src/hour3/Dice.java
+++ Hour 3/src/hour3/Dice.java
@@ -1,13 +1,22 @@
 package hour3;
 
 import java.util.*; 
-//importing all packages from the utility class in java.  the * is used to import all
+//importing all packages from the utility package in java.  the * is used to import all
+//no longer necessary to use the full name java.util.Random for the Random class
 
 class Dice {
 
 	public static void main(String[] args) {
-			Random generator = new Random();
-			int value = generator.nextInt();
+			Random blah = new Random();
+			//The Random class can be used to create Random objects.  used the 'new' keyword, followed by name of the class and parentheses
+			//this creates a variable named 'generator' that holds a 'new' Random object.
+			int value = blah.nextInt();
+			//for this program the object's 'nextInt' method will be employed to produce a random integer value
+			
+			
+			
+			
+			//there are far superior ways to actually get random numbers according to orcale's documentation
 			System.out.println("The random number is "
 					+ value);
 
