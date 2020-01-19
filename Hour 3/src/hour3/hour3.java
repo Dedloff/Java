4e0a529f36f8a5162d3824473952821bebdfe49f
Dedloff <dedloffn@gmail.com>
2020-01-19 16:26:57 -0500

    Simple example of Class utilization
    Hard coded number calling Math with method sqrt

---
+++ Hour 3/src/hour3/hour3.java
@@ -0,0 +1,17 @@
+package hour3;
+
+class hour3 {
+
+	public static void main(String[] args) {
+		int number = 225;
+		//Store an integer as 225 (hard coded)
+		System.out.println("The square root of "
+		+ number
+		+ " is "
+		+ Math.sqrt(number)
+		//using the Math class and calling the method sqrt, you can find the square root of the number
+	); 
+
+	}
+
+}
---
+++ Hour 3/src/module-info.java
@@ -0,0 +1,2 @@
+module hour3 {
+}
